9fddd7caf76abdf43b690c659b03c0c2a874d1d3
Doug Odegaard <doug@intralogix.biz>
2016-09-25 16:12:52 -0600

    Final Game

--- wordgame.py
+++ wordgame.py
@@ -6,7 +6,28 @@ def get_random_word():
     return random_word
 
 def show_word(word):
-    print(word)
+    for character in word:
+        print(character, " ", end="")
+    print("")
+
+def get_guess():
+    print("Enter a letter")
+    return input()
+
+def process_letter(letter, secret_word, blanked_word):
+    result = False
+
+    for i in range(0, len(secret_word)):
+        if secret_word[i] == letter:
+            result = True
+            blanked_word[i] = letter
+
+    return result
+
+def print_strikes(strikes):
+    for i in range(0, strikes):
+        print("X", end="")
+    print("")
 
 def play_word_games():
     strikes = 0
@@ -15,16 +36,22 @@ def play_word_games():
 
     word = get_random_word()
 
-    blanked_word = "_" * len(word)
+    blanked_word = list("_" * len(word))
 
     while playing:
         show_word(blanked_word)
 
-        #letter = get_guess()
+        letter = get_guess()
+        found = process_letter(letter, word, blanked_word)
 
-        strikes += 1
+        if not found:
+            strikes += 1
+            print_strikes(strikes)
 
         if strikes >= max_strikes:
+            playing = False
+
+        if not "_" in blanked_word:
             playing = False
         
     if strikes >= max_strikes:
