c38b3a118c95479385b98dad459de923ac09b043
Doug Odegaard <doug@intralogix.biz>
2016-09-25 14:28:41 -0600

    Added the playing loop and exits

--- wordgame.py
+++ wordgame.py
@@ -1,5 +1,24 @@
-def playWordGames():
-    print("hey world")
+def get_random_word():
+    return "pizza"
 
-print("playing")
-playWordGames()
+def play_word_games():
+    strikes = 0
+    max_strikes = 3
+    playing = True
+
+    word = get_random_word()
+
+    while playing:
+        strikes += 1
+
+        if strikes >= max_strikes:
+            playing = False
+        
+    if strikes >= max_strikes:
+        print("Sorry. Game over.")
+    else:
+        print("Winner!")
+    
+print("Start the Game")
+play_word_games()
+print("End the Game")
